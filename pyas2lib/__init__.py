de5f508ad9820eac38ae995fc8d190f1157e50e7
abhishekram <abhishek8816@gmail.com>
2018-02-15 17:01:23 +0530

    make attrs easily available

--- pyas2lib/__init__.py
+++ pyas2lib/__init__.py
@@ -1,10 +1,23 @@
 from __future__ import absolute_import
 import sys
+from pyas2lib.as2 import DIGEST_ALGORITHMS, ENCRYPTION_ALGORITHMS,\
+    MDN_CONFIRM_TEXT, MDN_FAILED_TEXT, Partner, Organization, Message, MDN
 
-VERSION = (1, 0, '0b1')
+VERSION = (1, 0, 0)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
+__all__ = [
+    'VERSION',
+    'DIGEST_ALGORITHMS',
+    'ENCRYPTION_ALGORITHMS',
+    'MDN_CONFIRM_TEXT',
+    'MDN_FAILED_TEXT',
+    'Partner',
+    'Organization',
+    'Message',
+    'MDN'
+]
 
 if (2, 7) <= sys.version_info < (3, 2):
     # On Python 2.7 and Python3 < 3.2, install no-op handler to silence
