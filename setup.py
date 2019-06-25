50dcec0cb8adc3bab6ffa2da0168841be31301f8
abhishekram <abhishek8816@gmail.com>
2019-06-25 17:40:00 +0530

    Handle MDNNotfound correctly when parsing an mdn

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     long_description="Docs for this project are maintained at "
                      "https://github.com/abhishek-ram/pyas2-lib/blob/"
                      "master/README.md",
-    version='1.2.2s',
+    version='1.2.2',
     author="Abhishek Ram",
     author_email="abhishek8816@gmail.com",
     packages=find_packages(
