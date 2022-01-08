474bbf5d11fb4e61ec066246619becef3d408db8
abhishekram <abhishek8816@gmail.com>
2022-01-08 09:31:15 +0530

    Add support for 3.10

--- setup.py
+++ setup.py
@@ -37,6 +37,8 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Security :: Cryptography",
         "Topic :: Communications",
     ],
